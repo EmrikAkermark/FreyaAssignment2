7c420f970832b03c24d707a298e4acf9e7286e23
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-09-28 19:11:10 +0200

    Initial Reflection Work

---
+++ Assets/Scripts/LaserReflections.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LaserReflections : MonoBehaviour
+{
+	public int MaxBounces = 4;
+
+	private void OnDrawGizmos()
+	{
+		RaycastHit hitThing;
+		if (Physics.Raycast(transform.position, transform.forward, out hitThing, 1000f))
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine(transform.position, hitThing.point);
+		}
+		else
+		{
+			DrawMiss();
+		}
+		void DrawMiss()
+		{
+				Gizmos.color = Color.green;
+				Gizmos.DrawLine(transform.position, transform.position + (transform.forward * 10));
+		}
+	}
+
+	void DrawLaser(Vector3 position, Vector3 rotation, int bounces)
+	{
+		if(bounces >= MaxBounces)
+		{
+			return;
+		}
+	}
+}
