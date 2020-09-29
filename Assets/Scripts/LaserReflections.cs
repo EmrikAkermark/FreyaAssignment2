5daea99ad484f0b0aa6e09405d04081172ee3a79
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-09-29 12:54:49 +0200

    Reflections!

--- Assets/Scripts/LaserReflections.cs
+++ Assets/Scripts/LaserReflections.cs
@@ -10,52 +10,48 @@ public class LaserReflections : MonoBehaviour
 	private void OnDrawGizmos()
 	{
 
-		if (Physics.Raycast(transform.position, transform.forward, 1000f))
-		{
-			DrawLaser(transform.position, transform.forward, 0);
-		}
-		else
-		{
-			DrawMiss();
-		}
+		DrawLaser(transform.position, transform.forward);
 		
 	}
 
-	void DrawMiss()
+	void DrawMiss(Vector3 startPosition, Vector3 Angle)
 	{
-		Gizmos.color = Color.green;
-		Gizmos.DrawLine(transform.position, transform.position + (transform.forward * 10));
+		Gizmos.color = Color.white;
+		Gizmos.DrawLine(startPosition, startPosition + Angle * 10);
 	}
-	void DrawLaser(Vector3 position, Vector3 rotation, int maxBounces)
+	void DrawLaser(Vector3 position, Vector3 angle)
 	{
-		if(maxBounces >= MaxBounces)
-		{
-			return;
-		}
-        //for (int i = 0; i < maxBounces; i++)
-        //{
-
-        //}
 		RaycastHit hitThing;
-		Physics.Raycast(position, rotation, out hitThing, 1000f);
-		Vector3 normal = hitThing.normal;
-		Vector3 hitPoint = hitThing.point;
-		//I can feel my brain growing
-		Vector3 thatWeirdDotAngle = Vector3.Dot(normal, rotation) * normal;
 
-		//Vector3 isThisTheReflection = thatWeirdDotAngle * -2;
+		for (int i = 0; i < MaxBounces; i++)
+        {
+			if (!Physics.Raycast(position, angle, out hitThing, 1000f))
+            {
+				DrawMiss(position, angle);
+				return;
+            }
+			Vector3 normal = hitThing.normal;
+			Vector3 hitPoint = hitThing.point;
+			//I can feel my brain growing
+			Vector3 thatWeirdDotAngle = Vector3.Dot(normal, angle) * normal;
 
-		Gizmos.color = Color.red;
-		Gizmos.DrawLine(position, hitThing.point);
-		Gizmos.color = Color.yellow;
-		Gizmos.DrawLine(hitPoint, hitPoint + normal);
-		Gizmos.color = Color.green;
-		Gizmos.DrawLine(hitPoint + rotation, (hitPoint - thatWeirdDotAngle * 2 + rotation));
-		Gizmos.color = Color.magenta;
-		Gizmos.DrawLine(hitPoint, hitPoint + rotation - thatWeirdDotAngle * 2);
-		Gizmos.color = Color.cyan;
-		Gizmos.DrawLine(hitPoint, hitPoint + thatWeirdDotAngle);
-		Gizmos.color = Color.red;
-		Gizmos.DrawLine(hitPoint, hitPoint + rotation);
+			//Vector3 isThisTheReflection = thatWeirdDotAngle * -2;
+
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine(position, hitThing.point);
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawLine(hitPoint, hitPoint + normal);
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(hitPoint + angle, (hitPoint - thatWeirdDotAngle * 2 + angle));
+			Gizmos.color = Color.magenta;
+			Gizmos.DrawLine(hitPoint, hitPoint + angle - thatWeirdDotAngle * 2);
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawLine(hitPoint, hitPoint + thatWeirdDotAngle);
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine(hitPoint, hitPoint + angle);
+			position = hitPoint;
+			angle = angle + thatWeirdDotAngle * -2;
+
+		}
 	}
 }
