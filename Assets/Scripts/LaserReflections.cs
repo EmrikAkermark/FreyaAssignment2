0d8ab58b9ddacc07fc1ce7c9333db63ae8f02b8b
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-09-29 12:42:08 +0200

    Made the laser reflection work!

--- Assets/Scripts/LaserReflections.cs
+++ Assets/Scripts/LaserReflections.cs
@@ -26,18 +26,36 @@ public class LaserReflections : MonoBehaviour
 		Gizmos.color = Color.green;
 		Gizmos.DrawLine(transform.position, transform.position + (transform.forward * 10));
 	}
-	void DrawLaser(Vector3 position, Vector3 rotation, int bounces)
+	void DrawLaser(Vector3 position, Vector3 rotation, int maxBounces)
 	{
-		if(bounces >= MaxBounces)
+		if(maxBounces >= MaxBounces)
 		{
 			return;
 		}
+        //for (int i = 0; i < maxBounces; i++)
+        //{
+
+        //}
 		RaycastHit hitThing;
 		Physics.Raycast(position, rotation, out hitThing, 1000f);
+		Vector3 normal = hitThing.normal;
+		Vector3 hitPoint = hitThing.point;
+		//I can feel my brain growing
+		Vector3 thatWeirdDotAngle = Vector3.Dot(normal, rotation) * normal;
+
+		//Vector3 isThisTheReflection = thatWeirdDotAngle * -2;
+
 		Gizmos.color = Color.red;
 		Gizmos.DrawLine(position, hitThing.point);
-
-
-
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(hitPoint, hitPoint + normal);
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(hitPoint + rotation, (hitPoint - thatWeirdDotAngle * 2 + rotation));
+		Gizmos.color = Color.magenta;
+		Gizmos.DrawLine(hitPoint, hitPoint + rotation - thatWeirdDotAngle * 2);
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawLine(hitPoint, hitPoint + thatWeirdDotAngle);
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(hitPoint, hitPoint + rotation);
 	}
 }
