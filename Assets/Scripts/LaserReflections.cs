a249dc39ed670ee3d8ca9ac3e8cfdb6b855f464f
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-09-29 14:15:41 +0200

    Laser Is a little borked now

--- Assets/Scripts/LaserReflections.cs
+++ Assets/Scripts/LaserReflections.cs
@@ -1,33 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 public class LaserReflections : MonoBehaviour
 {
 	public int MaxBounces = 4;
+	public LineRenderer LaserLines;
+	public Vector3[] Laserpoints = new Vector3[5];
 
+
+    private void Start()
+    {
+		DrawLaser(transform.position, transform.forward);
+		LaserLines.SetPositions(Laserpoints);
+    }
+#if UNITY_EDITOR
 	private void OnDrawGizmos()
 	{
 
 		DrawLaser(transform.position, transform.forward);
+
 		
 	}
+#endif
 
 	void DrawMiss(Vector3 startPosition, Vector3 Angle)
 	{
-		Gizmos.color = Color.magenta;
+		Gizmos.color = Color.red;
 		Gizmos.DrawLine(startPosition, startPosition + Angle * 10);
 	}
 	void DrawLaser(Vector3 position, Vector3 angle)
 	{
 		RaycastHit hitThing;
-
+		Laserpoints[4] = position;
 		for (int i = 0; i < MaxBounces; i++)
         {
 			if (!Physics.Raycast(position, angle, out hitThing, 1000f))
             {
+#if UNITY_EDITOR
 				DrawMiss(position, angle);
+#endif
 				return;
             }
 			Vector3 normal = hitThing.normal;
@@ -35,9 +50,10 @@ public class LaserReflections : MonoBehaviour
 			//I can feel my brain growing
 			Vector3 thatWeirdDotAngle = Vector3.Dot(normal, angle) * normal;
 			Vector3 theReflection = thatWeirdDotAngle * -2;
-
+#if UNITY_EDITOR
 			DrawThemGizmos(position, hitPoint, hitThing.normal, angle, theReflection, thatWeirdDotAngle);
-
+#endif
+			Laserpoints[3 - i] = hitPoint;
 			position = hitPoint;
 			angle += theReflection;
 
@@ -52,11 +68,11 @@ public class LaserReflections : MonoBehaviour
 		Gizmos.DrawLine(rayHitPosition, rayHitPosition + normal);
 		Gizmos.color = Color.green;
 		Gizmos.DrawLine(rayHitPosition + initialAngle, (rayHitPosition + ReflectionAngle + initialAngle));
-		Gizmos.color = Color.magenta;
+		Gizmos.color = Color.red;
 		Gizmos.DrawLine(rayHitPosition, rayHitPosition + initialAngle + ReflectionAngle);
 		Gizmos.color = Color.cyan;
 		Gizmos.DrawLine(rayHitPosition, rayHitPosition + DotProductAngle);
-		Gizmos.color = Color.red;
+		Gizmos.color = Color.magenta;
 		Gizmos.DrawLine(rayHitPosition, rayHitPosition + initialAngle);
 	}
 }
