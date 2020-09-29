389d490b9fdbabf2a5ec0e540a6da7aec9327825
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-09-29 16:43:45 +0200

    Cleaned Up Lasers

--- Assets/Scripts/LaserReflections.cs
+++ Assets/Scripts/LaserReflections.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class LaserReflections : MonoBehaviour
 {
@@ -12,12 +9,11 @@ public class LaserReflections : MonoBehaviour
 	public Vector3[] Laserpoints = new Vector3[5];
 
 
-    private void Start()
-    {
-		DrawLaser(transform.position, transform.forward);
-		LaserLines.SetPositions(Laserpoints);
-    }
-#if UNITY_EDITOR
+  //  private void Start()
+  //  {
+		//DrawLaser(transform.position, transform.forward);
+		//LaserLines.SetPositions(Laserpoints);
+  //  }
 	private void OnDrawGizmos()
 	{
 
@@ -25,13 +21,7 @@ public class LaserReflections : MonoBehaviour
 
 		
 	}
-#endif
 
-	void DrawMiss(Vector3 startPosition, Vector3 Angle)
-	{
-		Gizmos.color = Color.red;
-		Gizmos.DrawLine(startPosition, startPosition + Angle * 10);
-	}
 	void DrawLaser(Vector3 position, Vector3 angle)
 	{
 		RaycastHit hitThing;
@@ -40,9 +30,7 @@ public class LaserReflections : MonoBehaviour
         {
 			if (!Physics.Raycast(position, angle, out hitThing, 1000f))
             {
-#if UNITY_EDITOR
 				DrawMiss(position, angle);
-#endif
 				return;
             }
 			Vector3 normal = hitThing.normal;
@@ -50,9 +38,7 @@ public class LaserReflections : MonoBehaviour
 			//I can feel my brain growing
 			Vector3 thatWeirdDotAngle = Vector3.Dot(normal, angle) * normal;
 			Vector3 theReflection = thatWeirdDotAngle * -2;
-#if UNITY_EDITOR
 			DrawThemGizmos(position, hitPoint, hitThing.normal, angle, theReflection, thatWeirdDotAngle);
-#endif
 			Laserpoints[3 - i] = hitPoint;
 			position = hitPoint;
 			angle += theReflection;
@@ -60,6 +46,13 @@ public class LaserReflections : MonoBehaviour
 		}
 	}
 
+	void DrawMiss(Vector3 startPosition, Vector3 Angle)
+	{
+		Gizmos.color = Color.magenta;
+		Gizmos.DrawLine(startPosition, startPosition + Angle * 10);
+	}
+
+	//Gaze upon my pride and joy.
 	void DrawThemGizmos(Vector3 originPosition, Vector3 rayHitPosition, Vector3 normal, Vector3 initialAngle, Vector3 ReflectionAngle, Vector3 DotProductAngle)
     {
 		Gizmos.color = Color.red;
