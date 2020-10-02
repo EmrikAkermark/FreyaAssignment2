a889a6313aff8ce4461807d45e42f3aa28305777
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-10-02 16:21:06 +0200

    Clean Torus

--- Assets/Scripts/Coils.cs
+++ Assets/Scripts/Coils.cs
@@ -13,22 +13,20 @@ public class Coils : MonoBehaviour
     [Min(2)]
     public int Corners;
 
-    public Vector3[] CornerPositions, CirclePositions, CircleRights;
-
     private void OnDrawGizmos()
     {
         int totalCorners = Corners * Rotations;
         float angle = Mathf.PI * 2 / totalCorners;
-        CornerPositions = new Vector3[totalCorners];
-        CirclePositions = new Vector3[totalCorners];
-        CircleRights = new Vector3[totalCorners];
+        Vector3[] TorusPositions = new Vector3[totalCorners];
+        Vector3[] CirclePositions = new Vector3[totalCorners];
+        Vector3[] CircleRights = new Vector3[totalCorners];
         for (int i = 0; i < totalCorners; i++)
         {
 
             Vector3 circlePosition = new Vector3(Mathf.Sin(angle * i) * CircleRadius, 0, Mathf.Cos(angle * i) * CircleRadius);
             CirclePositions[i] = circlePosition;
             CircleRights[i] = circlePosition.normalized;
-            CornerPositions[i] = new Vector3(Mathf.Sin(angle * i * Rotations) * SpiralRadius, Mathf.Cos(angle * i * Rotations) * SpiralRadius, 0);
+            TorusPositions[i] = (CircleRights[i] * Mathf.Cos(angle * i * Rotations) + Vector3.up * Mathf.Sin(angle * i * Rotations)) * SpiralRadius;
 
 
         }
@@ -41,7 +39,7 @@ public class Coils : MonoBehaviour
         {
             lerpValue = (float)i / totalCorners;
             Gizmos.color = Color.Lerp(Color.cyan, Color.magenta, lerpValue);
-            Gizmos.DrawWireSphere(CirclePositions[i], 0.5f);
+            Gizmos.DrawWireSphere(CirclePositions[i], 0.3f);
             Vector3 rightAngle = CircleRights[i];
             Gizmos.DrawLine(CirclePositions[i], CirclePositions[i] + rightAngle);
 
@@ -52,21 +50,21 @@ public class Coils : MonoBehaviour
         {
             lerpValue = (float)i / totalCorners;
             Gizmos.color = Color.Lerp(Color.green, Color.blue, lerpValue);
-            Vector3 x = CircleRights[i] * Mathf.Cos(angle * i * Rotations) * SpiralRadius;
-            Vector3 y = Vector3.up * Mathf.Sin(angle * i * Rotations) * SpiralRadius;
-            Gizmos.DrawWireSphere(CirclePositions[i] + x + y, 0.2f);
+            Gizmos.DrawWireSphere(CirclePositions[i] + TorusPositions[i], 0.2f);
             //Gizmos.DrawWireSphere(CornerPositions[i] + Vector3.forward * i + Vector3.forward * 10, 0.5f);
         }
 
-        for (int i = 0; i < totalCorners - 1; i++)
+        for (int i = 0; i < totalCorners; i++)
         {
             int start = i % totalCorners;
             int next = (i + 1) % totalCorners;
             lerpValue = (float)i / totalCorners;
             Gizmos.color = Color.Lerp(Color.red, Color.green, lerpValue);
+            Gizmos.DrawLine(TorusPositions[start], TorusPositions[next]);
+            Gizmos.color = Color.Lerp(Color.blue, Color.green, lerpValue);
+            Gizmos.DrawLine(CirclePositions[start] + TorusPositions[start], CirclePositions[next] + TorusPositions[next]);
 
-            Gizmos.DrawLine(CornerPositions[start], CornerPositions[next]);
-            
+
         }
     }
 }
